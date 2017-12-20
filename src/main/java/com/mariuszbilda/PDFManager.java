14d81309797a4de93c7bd4712b6cec984357b0df
mariusz PC L <mariusz.mab@gmail.com>
2017-12-20 15:24:59 +0100

    Wrong scaling of the images fixed. The hightest part of the image was always cut.

--- src/main/java/com/mariuszbilda/PDFManager.java
+++ src/main/java/com/mariuszbilda/PDFManager.java
@@ -1,21 +1,27 @@
 package com.mariuszbilda;
 
+import com.sun.istack.internal.NotNull;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+import org.apache.pdfbox.printing.Scaling;
 
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PDFManager {
     private PDDocument doc = null;
+    private Logger logger;
 
     public PDFManager() {
         doc = new PDDocument();
+        logger = Logger.getLogger("PDF MANAGER");
     }
 
     /**
@@ -23,6 +29,7 @@ public class PDFManager {
      * This method take a path of one image and add it to the PDF document.
      * @param imagePath path of the image
      */
+    @NotNull
     public void addPage(String imagePath) {
 
 
@@ -33,20 +40,34 @@ public class PDFManager {
             int originalWidth = pdImageXObject.getWidth();
             int originalHeight = pdImageXObject.getHeight();
 
+            logger.log(Level.INFO, String.format("Original sizes: w: %d h: %d", originalWidth, originalHeight));
+
             float pageWidth = pageSize.getWidth();
             float pageHeight = pageSize.getHeight();
+
+            logger.log(Level.INFO, String.format("Page sizes: w: %.2f h: %.2f", pageWidth, pageHeight));
+
             float ratio = Math.min(pageWidth / originalWidth, pageHeight / originalHeight);
-            float scaledWidth = originalWidth * ratio;
-            float scaledHeight = originalHeight * ratio;
+
+            logger.log(Level.INFO, String.format("RATIO: %.2f", ratio));
+
+            float scaledWidth = originalWidth * ratio; // (pageWidth / originalWidth)
+            float scaledHeight = originalHeight  * ratio; //(pageHeight / originalHeight)
+
+            logger.log(Level.INFO, String.format("Scaled: w: %.2f h: %.2f", scaledWidth, scaledHeight));
 
             float x = (pageWidth - scaledWidth) / 2;
             float y = (pageHeight - scaledHeight) / 2;
 
-            PDPage page = new PDPage();
+            logger.log(Level.INFO, String.format("X: %.2f Y: %.2f", x, y));
+
+            PDPage page = new PDPage(pageSize);
             doc.addPage(page);
 
             try (PDPageContentStream contentStream = new PDPageContentStream(doc, page)) {
-                contentStream.drawImage(pdImageXObject, x, y, scaledWidth, scaledHeight);
+
+                contentStream.drawImage(pdImageXObject, x,y, scaledWidth, scaledHeight);
+
             }
 
 
@@ -57,6 +78,7 @@ public class PDFManager {
 
     public void savePDF(String destinationPath){
         try {
+            logger.log(Level.INFO, "Saving document into path: " + destinationPath);
             doc.save(destinationPath + String.format("/%s.pdf", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HHmmss"))));
             doc.close();
         } catch (IOException e) {
